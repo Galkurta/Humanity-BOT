d218a2d73414e9d04dffc3fb6568dee2797ffc79
nookie <herogaluhpyawai@gmail.com>
2024-11-12 12:42:04 +0700

    [Update] updated User-Agent header in HumanityClient to a more accurate and detailed value.

--- main.js
+++ main.js
@@ -88,7 +88,8 @@ class HumanityClient {
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
-      "user-agent": "Mozilla/5.0 Custom Client",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
     };
   }
 
